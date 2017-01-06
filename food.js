3bcfb3ea277ac1259a1be6e3b634a3058264fb7d
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-06 10:39:38 -0600

    dog foods now load into page via nested for loops

--- food.js
+++ food.js
@@ -17,19 +17,32 @@ var promise1 = new Promise(function (resolve, reject){
 promise1.then(function(data){
   dogfood = data;
   console.log(dogfood);
+  fillDogChart();
 })
 
 function fillDogChart() {
   var dogFoodHTML = "";
   dogFoodHTML += `<table>`
   dogFoodHTML += `<tr><th scope="col">Brand</th>`
-  dogFoodHTML += `<tr><th scope="col">Type</th>`
-  dogFoodHTML += `<tr><th scope="col">Volume</th>`
-  dogFoodHTML += `<tr><th scope="col">Price/th></tr>`
+  dogFoodHTML += `<th scope="col">Type</th>`
+  dogFoodHTML += `<th scope="col">Volume</th>`
+  dogFoodHTML += `<th scope="col">Price</th></tr>`
 
   // interate through the object to fill out the table
-  $(dogfood).each(function(i) {
-    $(dogfood[i]).each()
-  })
+  for(var i = 0; i < dogfood.dog_brands.length; i++) {
+    for (var j = 0; j < dogfood.dog_brands[i].types.length; j++) {
+      for(var k = 0; k < dogfood.dog_brands[i].types[j].volumes.length; k++) {
+        dogFoodHTML += `<tr><td>${dogfood.dog_brands[i].name}</td>`;
+        dogFoodHTML += `<td>${dogfood.dog_brands[i].types[j].type}</td>`;
+        dogFoodHTML += `<td>${dogfood.dog_brands[i].types[j].volumes[k].name}</td>`;
+        dogFoodHTML += `<td>${dogfood.dog_brands[i].types[j].volumes[k].price}</td></tr>`;
+        console.log("Price:", dogfood.dog_brands[i].types[j].volumes[k].price)
+      }
+    }
   }
+  //add closing table tag
+  dogFoodHTML += `</table>`
+  console.log(dogFoodHTML);
+  //add html to element on page
+  $(".dogFood").html(dogFoodHTML);
 }
